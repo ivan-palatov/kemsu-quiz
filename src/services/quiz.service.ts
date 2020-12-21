684ada10382bc8240eb0777cea6f5dd6c932d618
Ivan Palatov <ivanakazver3d@gmail.com>
2020-12-21 12:08:59 +0700

    fix: add comments to quiz.service.ts

--- src/services/quiz.service.ts
+++ src/services/quiz.service.ts
@@ -1,6 +1,9 @@
 import { shuffleArray } from '@/utils/shuffleArray'
 import { timeout } from '@/utils/timeout'
 import { IQuiz, QuestionType, QuizType } from '@/utils/types'
+import axios from 'axios'
+
+const BASE_URL = 'https://quiz-backe.herokuapp.com/api/v1'
 
 class QuizService {
   private _quizes: IQuiz[] = [
@@ -52,40 +55,59 @@ class QuizService {
     },
   ]
 
+  /* Запрос на создание квиза quiz: {name: 'Test #1', author: 'Pedro', questions: [
+    {text: 'Сколько см пипи?', type: 'WRITTEN', answer: '3'}
+    {text: 'Лучший в мире?', type: 'SINGLE', variants: [
+      {text: 'Папич', isTrue: true}, {text: 'Бабич', isTrue: false}
+    ]}
+  ]}
+  */
   public async createQuiz(quiz: any) {
     return true
   }
 
+  /* Запрос на редактирование квиза id: int, quiz: {name: 'Test #1', author: 'Pedro', questions: [
+    {text: 'Сколько см пипи?', type: 'WRITTEN', answer: '3'}
+    {text: 'Лучший в мире?', type: 'SINGLE', variants: [
+      {text: 'Папич', isTrue: true}, {text: 'Бабич', isTrue: false}
+    ]}
+  ]}
+  */
   public async editQuiz(id: number, quiz: any) {
     return true
   }
 
+  // получить все активные квизы для студента
   public async getPublicQuizes() {
-    await timeout()
-    return this._quizes
+    return (await axios.get(BASE_URL + '/getQuizes')).data
   }
 
+  // получить все квизы для препода (здесь по имени, можно по ид автора, без разницы)
   public async getQuizes(user: string) {
     await timeout()
     return this._quizes.filter((q) => q.author === user)
   }
 
-  // fetch quiz info without questions
+  // получить квиз вообще БЕЗ ВОПРОСОВ, хз, может и не нужен такой роут, могу из другого взять инфу
   public async getQuiz(uid: string) {
     await timeout()
     return this._quizes.find((quiz) => quiz.uid === uid)
   }
 
+  // получить квиз для учителя, с вопросами, вариантами ответа и правильными ответами
   public async getQuizForTeacher(id: number) {
     await timeout()
     return this._quizes.find((quiz) => quiz.id === id)
   }
 
+  // ну понятно, по ид квиз дропнуть, правда тут конечно без авторизации кекв получиться может, но да ладно
   public async deleteQuiz(id: number) {
     await timeout()
     return true
   }
 
+  // это роут для назначения квизов на определённую дату и время, чтоб автоматом стартовали
+  // но хз нужно ли это, или ну его нахуй. Лучше в конце самом делать
   public async appointQuiz(
     id: number,
     testDate: string,
@@ -96,11 +118,13 @@ class QuizService {
     return true
   }
 
+  // Это роут для того, чтоб квиз начать преподу, timeLimit - время в минутах на его прохождение
   public async startQuiz(id: number, timeLimit: number) {
     await timeout()
     return true
   }
 
+  // Это роут для получения студентами квиза со всеми его вопросами, но без правильных ответов
   public async getQuizQuestions(uid: string) {
     await timeout()
     const quiz = this._quizes.find((quiz) => quiz.uid === uid)
@@ -116,6 +140,8 @@ class QuizService {
 
     return quiz.questions
   }
+
+  // Еще нужны роуты для просмотра результатов преподом, ответа на вопрос учеником, ответа на сразу все вопросы учеником, завершение квиза преподом
 }
 
 export const quizService = new QuizService()
